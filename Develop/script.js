fe0a6bf2465f839c0425a42871a47f1009e86acf
JordanEngland <lcplengland@gmail.com>
2023-04-06 11:42:13 -0500

    added password function to generate length

--- Develop/script.js
+++ Develop/script.js
@@ -6,6 +6,13 @@ var uppercase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var numbers = '1234567890';
 var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
 
+function generatePassword(){
+  var passwordLength = parseInt(prompt('Enter password length(bewteen 8-128 characters):'));
+  if(isNaN(passwordLength)|| passwordLength <8 || passwordLength >128){
+    alert('Not a valid password length. Re-enter a number bewteen 8 & 128.');
+    return'';
+  }
+}
 
 // Write password to the #password input
 function writePassword() {
